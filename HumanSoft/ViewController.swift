b4bb659287bc9c39c315e33c342820c81991a49e
AzzA <azzaamahmoud3@gmail.com>
2021-02-02 02:22:50 +0200

    Initialise

---
+++ HumanSoft/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HumanSoft
+//
+//  Created by Azza on 02/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
